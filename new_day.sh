237e71ff8cac5c4969751f663c8d7c9093c6df63
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-01-15 17:04:36 +0100

    Add new_day script

---
+++ new_day.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+day=$1
+
+if [ -z $day ]
+then
+  day=$(ls | egrep -o "[0-9]+" | sort -nr | head -1)
+  day=${day##0}  # Remove leading zero to avoid octal error
+  ((day++))
+fi
+
+day=$(printf "%02d" $day)
+
+sed -i "/load_year\!/,/^$/ s/\(day[[:digit:]]\{2\}\))/\1, day$day)/" ../lib.rs
+
+cat << EOF > day$day.rs
+use crate::args::RunArgs;
+
+use std::fs::read_to_string;
+
+pub fn run(args: &RunArgs) -> i64 {
+    let data = read_to_string(&args.input_file).expect("Error opening input file");
+
+    match args.part {
+      1 => run_part1(data),
+      2 => run_part2(data),
+      3 => run_part3(data),
+      _ => unreachable!(),
+  }
+}
+
+fn run_part1(input: String) -> i64 {
+    0
+}
+
+fn run_part2(input: String) -> i64 {
+    0
+}
+
+fn run_part3(input: String) -> i64 {
+    0
+}
+EOF
