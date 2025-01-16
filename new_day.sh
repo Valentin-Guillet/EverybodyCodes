9ea8c3b547e95b684b0e6438a338ccd03cf943f3
Valentin-Guillet <guillet.valentin95240@gmail.com>
2025-01-16 18:42:36 +0100

    New day create input dir

--- new_day.sh
+++ new_day.sh
@@ -11,6 +11,9 @@ fi
 
 day=$(printf "%02d" $day)
 
+year=$(basename $(pwd))
+mkdir ../../input/$year/day$day
+
 sed -i "/load_year\!/,/^$/ s/\(day[[:digit:]]\{2\}\))/\1, day$day)/" ../lib.rs
 
 cat << EOF > day$day.rs
@@ -22,10 +25,10 @@ pub fn run(args: &RunArgs) -> i64 {
     let data = read_to_string(&args.input_file).expect("Error opening input file");
 
     match args.part {
-      1 => run_part1(data),
-      2 => run_part2(data),
-      3 => run_part3(data),
-      _ => unreachable!(),
+        1 => run_part1(data),
+        2 => run_part2(data),
+        3 => run_part3(data),
+        _ => unreachable!(),
   }
 }
 
